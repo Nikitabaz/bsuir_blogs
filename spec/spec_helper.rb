3e50c56ef2f924c8ca26bab46dd0044b7e625374
Nikita Bazylchuk <niktabasylschuk@gmail.com>
2015-10-01 13:14:29 +0300

    Finish layout and routes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,4 +42,5 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  config.include Rails.application.routes.url_helpers
 end
